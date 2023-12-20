6037e664dde647faf7c9a54fbf3ca01f1a92e4a3
Carl Jones <c.m.jones001@gmail.com>
2023-12-20 23:57:09 +0100

    Day 19, problem 1

---
+++ day_19/src/main.rs
@@ -0,0 +1,242 @@
+mod parsing;
+
+use std::collections::HashMap;
+
+fn main() {
+    let result_one = problem_one();
+    println!("Problem one: {}", result_one);
+}
+
+fn problem_one() -> u64 {
+    let input = include_str!("problem_text");
+    get_rating_numbers(input)
+}
+
+fn get_rating_numbers(input: &str) -> u64 {
+    let (rem, filters) = parsing::parse_filters_rows(input).unwrap();
+    let (_, parts) = parsing::parse_parts(rem.trim()).unwrap();
+
+    parts.into_iter().filter(|part| {get_final_bin(part, &filters) == Bin::Accept}).map(|part| {part.total()}).sum()
+}
+
+/// Returns the final bin that the part ends up in, this is either Accept or Reject
+///
+/// If the part ends up in a label bin, this function will loop until it finds
+/// an Accept or Reject bin
+fn get_final_bin(part: &Part, filters: &HashMap<Bin, FilterList>) -> Bin {
+    let starting_filter = filters.get(&Bin::new("in")).unwrap();
+    let mut current_filter = starting_filter;
+
+    loop {
+        let next_bin = part.get_resulting_bin(current_filter);
+        if next_bin == Bin::Accept || next_bin == Bin::Reject {
+            return next_bin;
+        }
+        current_filter = filters.get(&next_bin).unwrap();
+    }
+}
+
+#[derive(PartialEq, Eq, Debug)]
+struct Part {
+    x: u64,
+    m: u64,
+    a: u64,
+    s: u64,
+}
+
+impl Part {
+    fn new(x: u64, m: u64, a: u64, s: u64) -> Part {
+        Part { x, m, a, s }
+    }
+
+    /// Returns the bin that the part ends up in after this filter
+    fn get_resulting_bin(&self, filter_list: &FilterList) -> Bin {
+        filter_list.0.iter().find_map(|filter| {
+            if filter.test_part(self) {
+                Some(filter.bin())
+            } else {
+                None
+            }
+        }).unwrap_or(Bin::Reject)
+    }
+
+    fn total(&self) -> u64 {
+        self.x + self.m + self.a + self.s
+    }
+}
+
+#[derive(PartialEq, Eq, Debug, Hash, Clone)]
+enum Bin {
+    Accept,
+    Reject,
+    Label(String),
+}
+
+impl Bin {
+    fn new(s: &str) -> Bin {
+        match s {
+            "A" => Bin::Accept,
+            "R" => Bin::Reject,
+            x => Bin::Label(x.to_string()),
+        }
+    }
+}
+
+#[derive(PartialEq, Debug)]
+struct FilterList(Vec<Filter>);
+
+#[derive(PartialEq, Debug)]
+enum Filter {
+    GreaterThan(u64, PartType, Bin),
+    LessThan(u64, PartType, Bin),
+    Unconditional(Bin),
+}
+
+impl Filter {
+    fn new_greater_than(value: u64, part_type: PartType, bin: Bin) -> Filter {
+        Filter::GreaterThan(value, part_type, bin)
+    }
+
+    fn new_less_than(value: u64, part_type: PartType, bin: Bin) -> Filter {
+        Filter::LessThan(value, part_type, bin)
+    }
+
+    fn new_unconditional(bin: Bin) -> Filter {
+        Filter::Unconditional(bin)
+    }
+
+    fn bin(&self) -> Bin {
+        match self {
+            Filter::GreaterThan(_, _, bin) => bin.clone(),
+            Filter::LessThan(_, _, bin) => bin.clone(),
+            Filter::Unconditional(bin) => bin.clone(),
+        }
+    }
+
+    /// Returns true if the part passes the filter
+    fn test_part(&self, part: &Part) -> bool {
+        match self {
+            Filter::GreaterThan(value, part_type, _bin) => {
+                match part_type {
+                    PartType::X => part.x > *value,
+                    PartType::M => part.m > *value,
+                    PartType::A => part.a > *value,
+                    PartType::S => part.s > *value,
+                }
+            },
+            Filter::LessThan(value, part_type, _bin) => {
+                match part_type {
+                    PartType::X => part.x < *value,
+                    PartType::M => part.m < *value,
+                    PartType::A => part.a < *value,
+                    PartType::S => part.s < *value,
+                }
+            },
+            Filter::Unconditional(_bin) => true,
+        }
+    }
+}
+
+#[derive(PartialEq, Debug, Copy, Clone)]
+enum PartType {
+    X,
+    M,
+    A,
+    S,
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use test_case::test_case;
+    use indoc::indoc;
+
+    fn filter_string() -> &'static str {
+        indoc! {"
+            px{a<2006:qkq,m>2090:A,rfg}
+            pv{a>1716:R,A}
+            lnx{m>1548:A,A}
+            rfg{s<537:gd,x>2440:R,A}
+            qs{s>3448:A,lnx}
+            qkq{x<1416:A,crn}
+            crn{x>2662:A,R}
+            in{s<1351:px,qqz}
+            qqz{s>2770:qs,m<1801:hdj,R}
+            gd{a>3333:R,R}
+            hdj{m>838:A,pv}
+        "}
+    }
+
+    #[test]
+    fn test_parse_filter() {
+        let filter_str = filter_string();
+        let (_res, filter_dict) = parsing::parse_filters_rows(filter_str).unwrap();
+        let starting_filter = filter_dict.get(&Bin::new("in")).unwrap();
+
+        let part = Part::new(787, 2655, 1222, 2876);
+
+        let resulting_bin = part.get_resulting_bin(starting_filter);
+        let expected_bin = Bin::Label("qqz".to_string());
+
+        assert_eq!(resulting_bin, expected_bin);
+
+        let filter = filter_dict.get(&expected_bin).unwrap();
+        let expected_bin = Bin::Label("qs".to_string());
+
+        let resulting_bin = part.get_resulting_bin(filter);
+
+        assert_eq!(resulting_bin, expected_bin);
+
+        let filter = filter_dict.get(&expected_bin).unwrap();
+        let expected_bin = Bin::Label("lnx".to_string());
+
+        let resulting_bin = part.get_resulting_bin(filter);
+
+        assert_eq!(resulting_bin, expected_bin);
+
+        let filter = filter_dict.get(&expected_bin).unwrap();
+        let expected_bin = Bin::Accept;
+
+        let resulting_bin = part.get_resulting_bin(filter);
+
+        assert_eq!(resulting_bin, expected_bin);
+    }
+
+    #[test_case(787, 2655, 1222, 2876, Bin::Accept)]
+    #[test_case(1679, 44, 2067, 496, Bin::Reject)]
+    #[test_case(2036, 264, 79, 2244, Bin::Accept)]
+    fn test_final_bins(x: u64, m: u64, a: u64, s: u64, expected_bin: Bin) {
+        let part = Part::new(x, m, a, s);
+        let filter_str = filter_string();
+
+        let (_res, filter_dict) = parsing::parse_filters_rows(filter_str).unwrap();
+        let final_bin = get_final_bin(&part, &filter_dict);
+
+        assert_eq!(final_bin, expected_bin);
+    }
+
+    #[test]
+    fn sample_one() {
+        let input = indoc! {"
+            px{a<2006:qkq,m>2090:A,rfg}
+            pv{a>1716:R,A}
+            lnx{m>1548:A,A}
+            rfg{s<537:gd,x>2440:R,A}
+            qs{s>3448:A,lnx}
+            qkq{x<1416:A,crn}
+            crn{x>2662:A,R}
+            in{s<1351:px,qqz}
+            qqz{s>2770:qs,m<1801:hdj,R}
+            gd{a>3333:R,R}
+            hdj{m>838:A,pv}
+
+            {x=787,m=2655,a=1222,s=2876}
+            {x=1679,m=44,a=2067,s=496}
+            {x=2036,m=264,a=79,s=2244}
+            {x=2461,m=1339,a=466,s=291}
+            {x=2127,m=1623,a=2188,s=1013}
+        "};
+
+        assert_eq!(get_rating_numbers(input), 19114);
+    }
+}
---
+++ day_19/src/parsing.rs
@@ -0,0 +1,191 @@
+use nom::{IResult, Parser};
+use nom::character::complete::{alpha1, multispace1, multispace0};
+use std::collections::HashMap;
+use nom::sequence::{delimited, tuple};
+use nom::bytes::complete::tag;
+use nom::multi::separated_list1;
+use nom::branch::alt;
+use nom_supreme::ParserExt;
+use crate::{Bin, Filter, FilterList, Part, PartType};
+
+/// Parse a list of parts, each of the form `{x=787,m=2655,a=1222,s=2876}`
+pub(crate) fn parse_parts(input: &str) -> IResult<&str, Vec<Part>> {
+    use nom::character::complete::u64;
+    let x_parser = u64.preceded_by(tag("x="));
+    let m_parser = u64.preceded_by(tag("m="));
+    let a_parser = u64.preceded_by(tag("a="));
+    let s_parser = u64.preceded_by(tag("s="));
+
+    let tuple_parser = delimited(
+        tag("{"),
+        tuple((
+            x_parser.terminated(tag(",")),
+            m_parser.terminated(tag(",")),
+            a_parser.terminated(tag(",")),
+            s_parser,
+        )),
+        tag("}"),
+    );
+
+    let part_line_parser = tuple_parser
+        .map(|(x, m, a, s)| Part::new(x, m, a, s));
+
+    let mut parser = separated_list1(multispace1, part_line_parser).terminated(multispace0);
+    parser.parse(input)
+}
+
+/// Parse a list of filter rows, each of the form `px{a<2006:qkq,m>2090:A,rfg}`
+pub(crate) fn parse_filters_rows(input: &str) -> IResult<&str, HashMap<Bin, FilterList>> {
+    let bin_parser = alpha1.map(Bin::new);
+    let filter_brace = delimited(tag("{"), parse_filter_list, tag("}"));
+
+    let line_parser = tuple((bin_parser, filter_brace)).map(|(bin, filter_list)| {
+        let mut map = HashMap::new();
+        map.insert(bin, filter_list);
+        map
+    });
+
+    let mut parser = separated_list1(multispace1, line_parser).map(|lines| {
+        lines.into_iter().fold(HashMap::new(), |mut acc, line| {
+            acc.extend(line);
+            acc
+        })
+    });
+
+    parser.parse(input)
+}
+
+/// Parse a filter list of the form `a<2006:qkq,m>2090:A,rfg`
+fn parse_filter_list(input: &str) -> IResult<&str, FilterList> {
+    let filter_parser = alt((parse_operator, parse_unconditional));
+    let mut parser = separated_list1(tag(","), filter_parser).map(FilterList);
+    parser.parse(input)
+}
+
+/// Parse a filter of the form `rfg` or `A`
+fn parse_unconditional(input: &str) -> IResult<&str, Filter> {
+    let label_parser = alpha1;
+
+    let mut parser = label_parser.map(|label| Filter::new_unconditional(Bin::new(label)));
+
+    parser.parse(input)
+}
+
+/// Parse a filter of the form `a<2006:qkq`
+fn parse_operator(input: &str) -> IResult<&str, Filter> {
+    let part_type_parser = alt((
+        tag("x").value(PartType::X),
+        tag("m").value(PartType::M),
+        tag("a").value(PartType::A),
+        tag("s").value(PartType::S),
+    ));
+    let relation_parser = alt((tag(">"), tag("<")));
+    let value_parser = nom::character::complete::u64;
+
+    let bin_parser = alpha1.preceded_by(tag(":"));
+
+    let mut parser = tuple((part_type_parser, relation_parser, value_parser, bin_parser)).map(
+        |(part, relation, value, _bin)| {
+            let bin = Bin::new(_bin);
+            match relation {
+                ">" => Filter::new_greater_than(value, part, bin),
+                "<" => Filter::new_less_than(value, part, bin),
+                _ => panic!("Unknown relation"),
+            }
+        },
+    );
+
+    parser.parse(input)
+}
+
+#[cfg(test)]
+mod test {
+    use indoc::indoc;
+    use super::*;
+
+    #[test]
+    fn test_parse_filter() {
+        let input = "a<2006:qkq";
+
+        let (res, filter) = parse_operator(input).expect("Failed to parse filter");
+        if !res.is_empty() {
+            panic!("Did not parse entire input");
+        }
+
+        let expected = Filter::new_less_than(2006, PartType::A, Bin::new("qkq"));
+
+        assert_eq!(filter, expected);
+    }
+
+    #[test]
+    fn test_parse_filter_list() {
+        let input = "a<2006:qkq,m>2090:A,rfg";
+
+        let (_res, result) = parse_filter_list(input).unwrap();
+        let expected = FilterList(vec![
+            Filter::new_less_than(2006, PartType::A, Bin::new("qkq")),
+            Filter::new_greater_than(2090, PartType::M, Bin::Accept),
+            Filter::new_unconditional(Bin::new("rfg")),
+        ]);
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn parse_filter_commands() {
+        let input = indoc! {"
+            px{a<2006:qkq,m>2090:A,rfg}
+            pv{a>1716:R,A}"
+        };
+
+        let (_res, result) = parse_filters_rows(input).unwrap();
+        assert_eq!(_res, "");
+
+        let mut expected = HashMap::new();
+        expected.insert(
+            Bin::new("px"),
+            FilterList(vec![
+                Filter::new_less_than(2006, PartType::A, Bin::new("qkq")),
+                Filter::new_greater_than(2090, PartType::M, Bin::Accept),
+                Filter::new_unconditional(Bin::new("rfg")),
+            ]),
+        );
+        expected.insert(
+            Bin::new("pv"),
+            FilterList(vec![
+                Filter::new_greater_than(1716, PartType::A, Bin::Reject),
+                Filter::new_unconditional(Bin::Accept),
+            ]),
+        );
+
+        let keys = vec![Bin::new("px"), Bin::new("pv")];
+
+        for key in keys {
+            assert_eq!(
+                result.get(&key),
+                expected.get(&key),
+                "Key {:?} did not match",
+                key
+            );
+        }
+    }
+
+    #[test]
+    fn parse_parts_test() {
+        let input = indoc! {"
+            {x=787,m=2655,a=1222,s=2876}
+            {x=1679,m=44,a=2067,s=496}
+        "};
+
+
+        let (_res, result) = parse_parts(input).unwrap();
+        assert_eq!(_res, "");
+
+        let expected = vec![
+            Part::new(787, 2655, 1222, 2876),
+            Part::new(1679, 44, 2067, 496),
+        ];
+
+        assert_eq!(result, expected);
+    }
+}
